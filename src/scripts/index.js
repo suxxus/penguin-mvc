a9928140507196aaf8ae69bd70c25c52e3c205fc
suxxus <suxxus@outlook.com>
2017-08-15 23:35:25 +0200

    Created shouldImplement funtion
    to check for methods that view and model must implement

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,39 @@
 const controller = require('./penguin-controller');
 const view = require('./penguin-view');
-const model = require('./penguin-model')();
+const model = require('./penguin-model');
+
+const logError = (objName, method) => () => {
+  throw Error(`${objName} should implement ${method} method`);
+};
+
+// shouldImplement [] -> {x:s}
+const shouldImplement = (objName, list = []) => {
+  const obj = {};
+  list.forEach((item) => {
+    obj[item] = logError(objName, item);
+  });
+  return obj;
+};
 
 const modelWithUrl = () =>
-  Object.assign({},
-    model, { fetchData: model.fetchData('/api') });
+  Object.assign({ baseUrl: '/api' },
+    shouldImplement('model', [
+      'fetchData',
+      'changeIndex',
+      'subscribe',
+    ]),
+    model(),
+  );
 
 const viewWithRoot = () =>
-  Object.assign({},
-    view, { initPenguinView: view.initPenguinView({ id: 'root' }) });
+  Object.assign({ containerId: 'root' },
+    shouldImplement('view', [
+      'initPenguinView',
+      'penguinControlls',
+      'addEventClickListener',
+    ]),
+    view,
+  );
 
 const onDOMContentLoaded = () => {
   controller.initialize(modelWithUrl(), viewWithRoot());
